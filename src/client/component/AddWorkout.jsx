133084e902153240921a3181dcd101f256e58f56
Vy Bui <kvy.bui@gmail.com>
2022-05-29 22:48:29 -0700

    add navigation

---
+++ src/client/component/AddWorkout.jsx
@@ -0,0 +1,13 @@
+import React, { Component} from "react";
+
+class AddWorkout extends Component{
+    render(){
+        return(
+        <main style={{ padding: "1rem 0" }}>
+            <h2>Add New Workout</h2>
+        </main>
+        );
+    }
+}
+
+export default AddWorkout;
--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import App from "./pages/App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+import AddWorkout from "./component/AddWorkout.jsx";
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+  );
+  root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/AddWorkout" element={<AddWorkout />}/>
+            <Route path="/AllWorkouts" />
+            <Route path="/Settings" />
+        </Routes>
+  </BrowserRouter>
+  );
