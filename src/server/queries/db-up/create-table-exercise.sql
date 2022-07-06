81f67cf6a1e6c8bb84a38a0e6cb9526ff595b545
bsbhakti <70367592+bsbhakti@users.noreply.github.com>
2022-07-05 23:04:07 -0700

    Add files via upload

---
+++ src/server/queries/db-up/create-table-exercise.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Exercise (
+	exerciseID TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	target TEXT,
+	equipment TEXT, 
+	gifURL TEXT  DEFAULT 'https://tenor.com/view/not-available-gif-14901199'
+);
