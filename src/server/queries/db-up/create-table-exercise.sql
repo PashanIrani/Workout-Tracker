b0013fc391f183bbe3b8312ccf8d9a32dbd92f97
Pashan Irani <pashanirani@outlook.com>
2022-07-07 08:47:02 -0700

    store exercises in DB

--- src/server/queries/db-up/create-table-exercise.sql
+++ src/server/queries/db-up/create-table-exercise.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Exercise (
-	exerciseID TEXT PRIMARY KEY,
+	exercise_id TEXT PRIMARY KEY,
 	name TEXT NOT NULL,
 	target TEXT,
 	equipment TEXT, 
-	gifURL TEXT DEFAULT 'https://tenor.com/view/not-available-gif-14901199'
+	gif_url TEXT DEFAULT 'https://tenor.com/view/not-available-gif-14901199'
 );
