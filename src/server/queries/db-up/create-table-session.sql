a4d4d855505bdd4b1dbc696eae6af791e8a70cc3
Vy Bui <kvy.bui@gmail.com>
2022-07-07 23:40:41 -0700

    add foreign keys to scripts

--- src/server/queries/db-up/create-table-session.sql
+++ src/server/queries/db-up/create-table-session.sql
@@ -3,5 +3,6 @@ CREATE TABLE session(
 	session_time DATE,
 	user_id TEXT,
 	workout_id TEXT,
-	FOREIGN KEY (user_id) REFERENCES users
+	FOREIGN KEY (user_id) REFERENCES users,
+	FOREIGN KEY (workout_id) REFERENCES workout
 );
