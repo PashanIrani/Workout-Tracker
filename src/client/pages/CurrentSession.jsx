e64f819959a53a1b1cfed7a03e82add107f81d3e
Pashan Irani <pashanirani@outlook.com>
2022-07-24 12:30:09 -0700

    fixed order

--- src/client/pages/CurrentSession.jsx
+++ src/client/pages/CurrentSession.jsx
@@ -12,15 +12,18 @@ const CurrentSession = (props) => {
   useEffect(() => {
     let temp = {};
     let exercise_map = {};
-    console.log(props.workout);
+    let exercise_order = [];
+
+    workout.exercises.sort((a, b) => a.exercise_order - b.exercise_order);
+
     for (let exercise of workout.exercises) {
       temp[exercise.exercise_id] = [];
+      exercise_order.push(exercise.exercise_id);
       exercise_map[exercise.exercise_id] = exercise;
     }
 
     workout["exercise_map"] = exercise_map;
-    console.log(temp);
-    console.log(exercise_map);
+    workout["exercise_order"] = exercise_order;
     setSets(temp);
   }, []);
 
@@ -49,7 +52,7 @@ const CurrentSession = (props) => {
 
   return (
     <div className="Page CurrentSession">
-      {Object.keys(sets).map((key) => {
+      {workout["exercise_order"]?.map((key) => {
         return (
           <ExerciseSetsCard
             key={key}
