fb7323d1e3d7dee8bcb7a24775a4582edd6d3394
bsbhakti <70367592+bsbhakti@users.noreply.github.com>
2022-07-26 11:57:00 -0700

    Update App.jsx

--- src/client/pages/App.jsx
+++ src/client/pages/App.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import "../styles/App.scss";
+import "../styles/AddWorkout.scss";
 import { Outlet, Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Button, Navbar } from "react-bootstrap";
 import TopNav from "../component/TopNav";
+import LoadSession from "./LoadSession";
 
 class App extends Component {
+  
   render() {
+    // change this id here to the id in your db for testing
+    var id = "SuxAjh";
+    // change this to the id of the user
+    var userId = "qw3Pmq"
     return (
       <div className="Page">
         <h1>Workout Tracker 💪</h1>
         <a href="/App/AddWorkout/"> Add Workout </a>
         <a href="/App/MyWorkouts/"> All Workouts </a>
+        <div style={{ display: "flex", justifyContent: 'center'}}>
+          <button onClick={()=>(location.href = "/App/MyWorkouts/")}> Start Workout </button>
+          </div>
+          <div onClick={()=>(location.href=`/App/SessionStats?id=${id}`)}>Session Stats</div> 
+          <h2> Previous Sessions </h2>
+         <LoadSession></LoadSession>
+        {/* for testing purposes; id from dashboard should also be passed this way */}
+      
         <Outlet />
       </div>
     );
