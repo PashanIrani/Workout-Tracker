668cfd947b3149292601a54fbbfe204f9243dfa2
Pashan Irani <pashanirani@outlook.com>
2022-07-27 13:08:06 -0700

    implemented delete functionality for sets

--- src/client/component/ExerciseSetsCard.jsx
+++ src/client/component/ExerciseSetsCard.jsx
@@ -3,20 +3,24 @@ import { useEffect } from "react";
 import "../styles/CurrentSession.scss";
 import SetCard from "./SetCard.jsx";
 import { IoMdAddCircle } from "react-icons/io";
+import ShortUniqueId from "short-unique-id";
 
 const ExerciseSetsCard = (props) => {
   let { info } = props;
   const [showingInfo, setShowingInfo] = useState(false);
   const [currentSets, setCurrentSets] = useState([]);
 
+  const uid = new ShortUniqueId({ length: 10 });
+
   const addNewSet = () => {
     currentSets.push({
+      id: uid(),
       weight:
         currentSets.length >= 1
           ? currentSets[currentSets.length - 1].weight
           : 0,
       reps:
-        currentSets.length >= 1 ? currentSets[currentSets.length - 1].reps : 0,
+        currentSets.length >= 1 ? currentSets[currentSets.length - 1].reps : 1,
     });
 
     setCurrentSets([...currentSets]);
@@ -37,14 +41,20 @@ const ExerciseSetsCard = (props) => {
         <div className="sets-container">
           {currentSets.length > 0 ? (
             currentSets.map((set, index) => {
+              console.log(set.id);
               return (
                 <SetCard
-                  key={index}
+                  id={set.id}
+                  key={set.id}
                   index={index}
                   weight={set.weight}
                   reps={set.reps}
                   onUpdate={(data) => {
                     currentSets[index] = { ...data };
+                    setCurrentSets([...currentSets]);
+                  }}
+                  onDelete={(index) => {
+                    currentSets.splice(index, 1);
                     setCurrentSets([...currentSets]);
                   }}
                 ></SetCard>
